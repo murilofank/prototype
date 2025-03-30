cb3d82b135a9ea91a6f23f9d4a99873e5823929c
Murilo Fank <murilo.fank@universo.univates.br>
2025-03-30 19:19:50 -0300

    core: add db seed

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO "tarefa" (descricao, data_criacao, data_prevista, data_encerramento, situacao) VALUES
+('Estudar JavaScript', NOW(), '2025-04-01', NULL, 'pendente'),
+('Finalizar relatório', NOW(), '2025-04-02', NULL, 'pendente'),
+('Treinar na academia', NOW(), '2025-04-03', NULL, 'pendente'),
+('Revisar código do projeto', NOW(), '2025-04-04', NULL, 'pendente'),
+('Fazer compras', NOW(), '2025-04-05', NULL, 'pendente'),
+('Preparar apresentação', NOW(), '2025-04-06', NULL, 'pendente'),
+('Marcar consulta médica', NOW(), '2025-04-07', NULL, 'pendente'),
+('Assistir curso online', NOW(), '2025-04-08', NULL, 'pendente'),
+('Organizar documentos', NOW(), '2025-04-09', NULL, 'pendente'),
+('Ler um livro', NOW(), '2025-04-10', NULL, 'pendente');
